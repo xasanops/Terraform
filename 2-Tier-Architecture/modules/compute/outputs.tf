11cd15fdcccec0430b3b38ab34b59337b7d4bcc2
Leeban Xasan <leeban.xasn@outlook.com>
2023-05-31 17:47:10 +0100

    removed

--- 2-Tier-Architecture/modules/compute/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "asg_app_tier" {
-  value = aws_autoscaling_group.app_tier
-}
--- 2-Tier-Architecture/modules/loadbalancer/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "alb_sg" {
-  value = aws_security_group.alb.id
-}
-
-output "alb_tg_arn" {
-  value = aws_lb_target_group.target_group.arn
-}
