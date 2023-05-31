71d987cbed9ac226828648d44a129d117a6bdb0a
Liibaan Xasan <75026899+xasanops@users.noreply.github.com>
2023-05-31 17:25:26 +0100

    Delete 2-Tier-Architecture directory

--- 2-Tier-Architecture/environment/install_apache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "Hello World! from $(hostname -f)" > /var/www/html/index.html
--- 2-Tier-Architecture/modules/network/variables.tf
+++
@@ -1,7 +0,0 @@
-# -- Network Module Variables ✍🏽 -- #
-
-variable "vpc_cidr" {}
-variable "environment" {}
-variable "vpc_name" {}
-variable "public_subnet_cidr" {}
-variable "private_subnet_cidr" {}
