ba0276aa0d4757f29e00bb3a175e070c6f31cdf0
Leeban <leeban.xasan@outlook.com>
2023-09-23 17:02:06 +0100

    Comment out VarFiles

--- DevOps Project/test/main_test.go
+++ DevOps Project/test/main_test.go
@@ -10,7 +10,7 @@ func TestDev(t *testing.T) {
 	t.Parallel()
 	terraformOptions := terraform.WithDefaultRetryableErrors(t,&terraform.Options{
 		TerraformDir: "../environment/dev",
-		VarFiles: []string{"secrets.tfvars"},
+		// VarFiles: []string{"secrets.tfvars"},
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -22,7 +22,7 @@ func TestStage(t *testing.T) {
 	t.Parallel()
 	terraformOptions := terraform.WithDefaultRetryableErrors(t,&terraform.Options{
 		TerraformDir: "../environment/stage", 
-		VarFiles: []string{"secrets.tfvars"},
+		// VarFiles: []string{"secrets.tfvars"},
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -34,21 +34,10 @@ func TestProd(t *testing.T) {
 	t.Parallel()
 	terraformOptions := terraform.WithDefaultRetryableErrors(t,&terraform.Options{
 		TerraformDir: "../environment/prod", 
-		VarFiles: []string{"secrets.tfvars"},
+		// VarFiles: []string{"secrets.tfvars"},
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
 }
-
-
-/* 
-If instances are in public subnets you can do the following 👇🏽
-	"fmt" "testing" "time"
-	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
-	
-	publicIP := terraform.Output(t, terraformOptions, "public_ip")
-	url := fmt.Sprintf("http://%s", publicIP)
-	http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello World", 5, 3*time.Second)
-*/
